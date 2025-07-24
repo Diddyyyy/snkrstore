0f76f54adaf6499b86c8a16a8e81eea1e963eca0
Diddyyyy <adrianwilson3409@gmail.com>
2025-07-24 15:09:40 +0800

    Add files via upload

---
+++ admin.cs
@@ -0,0 +1,28 @@
+a{
+    position: fixed;
+    top : 20px;
+    right : 20px;
+}
+body{
+    background-colour: rgd(213,205,205)
+}
+table {
+    width: 100%
+    border-radius: 8px;
+    overflow:hidden;
+    background-colour : azure;
+}
+th {
+    border; 1px solid #070707;
+    pading:12px;
+    background-colour: #000000c7;
+    colour: white
+}
+td {
+    border:0.5px solid #111111
+}
+h2 {
+    font-size: 28px;
+    colour: #333;
+    text-align: center;
+}
